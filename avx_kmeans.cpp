94bd713d16280e586e4b43b22ae077b692fa41d8
yuehaowang <wangyuehao1999@gmail.com>
2019-05-29 10:14:46 +0800

    Added avx kmeans.

---
+++ avx_kmeans.cpp
@@ -0,0 +1,326 @@
+////////////////
+// 
+// File: kmeans.cpp
+//
+//  Main body of K-Means simulaton. Reads in the original data points from
+//  `ori.txt`, performs K-Means clustering on randomly-picked initial
+//  centers, and writes the results into `res.txt` with the same format.
+//
+//  * You may (and should) include some extra headers for optimizations.
+//
+//  * You should and ONLY should modify the function body of `kmeans()`.
+//    DO NOT change any other exitsing part of the program.
+//
+//  * You may add your own auxiliary functions if you wish. Extra declarations
+//    can go in `kmeans.h`.
+//
+// Jose @ ShanghaiTech University
+//
+////////////////
+
+#include <fstream>
+#include <limits>
+#include <math.h>
+#include <chrono>
+#include "kmeans.h"
+
+
+/*********************************************************
+        Your extra headers and static declarations
+ *********************************************************/
+#include <cstring>
+#include <cassert>
+#include <omp.h>
+/*********************************************************
+                           End
+ *********************************************************/
+
+
+/*
+ * Entrance point. Time ticking will be performed, so it will be better if
+ *   you have cleared the cache for precise profiling.
+ *
+ */
+int
+main (int argc, char *argv[])
+{
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " <input.txt> <output.txt>"
+                  << std::endl;
+        return -1;
+    }
+    if (!(bool)std::ifstream(argv[1])) {
+        std::cerr << "ERROR: Data file " << argv[1] << " does not exist!"
+                  << std::endl;
+        return -1;
+    }
+    if ((bool)std::ifstream(argv[2])) {
+        std::cerr << "ERROR: Destination " << argv[2] << " already exists!"
+                  << std::endl;
+        return -1;
+    }
+    FILE *fi = fopen(argv[1], "r"), *fo = fopen(argv[2], "w");
+    
+    /* From `ori.txt`, acquire dataset size, number of colors (i.e. K in
+       K-Means),and read in all data points into static array `data`. */
+    int pn, cn;
+
+    assert(fscanf(fi, "%d / %d\n", &pn, &cn) == 2);
+
+    point_t * const data = new point_t[pn];
+    color_t * const coloring = new color_t[pn];
+
+    for (int i = 0; i < pn; ++i)
+        coloring[i] = 0;
+
+    int i = 0, c;
+    double x, y;
+
+    while (fscanf(fi, "%lf, %lf, %d\n", &x, &y, &c) == 3) {
+        data[i++].setXY(x, y);
+        if (c < 0 || c >= cn) {
+            std::cerr << "ERROR: Invalid color code encoutered!"
+                      << std::endl;
+            return -1;
+        }
+    }
+    if (i != pn) {
+        std::cerr << "ERROR: Number of data points inconsistent!"
+                  << std::endl;
+        return -1;
+    }
+
+    /* Generate a random set of initial center points. */
+    point_t * const mean = new point_t[cn];
+
+    srand(5201314);
+    for (int i = 0; i < cn; ++i) {
+        int idx = rand() % pn;
+        mean[i].setXY(data[idx].getX(), data[idx].getY());
+    }
+
+    /* Invode K-Means algorithm on the original dataset. It should cluster
+       the data points in `data` and assign their color codes to the
+       corresponding entry in `coloring`, using `mean` to store the center
+       points. */
+    std::cout << "Doing K-Means clustering on " << pn
+              << " points with K = " << cn << "..." << std::flush;
+    auto ts = std::chrono::high_resolution_clock::now();
+    kmeans(data, mean, coloring, pn, cn);
+    auto te = std::chrono::high_resolution_clock::now();
+    std::cout << "done." << std::endl;
+    std::cout << " Total time elapsed: "
+              << std::chrono::duration_cast<std::chrono::milliseconds> \
+                 (te - ts).count()
+              << " milliseconds." << std::endl; 
+
+    /* Write the final results to `res.txt`, in the same format as input. */
+    fprintf(fo, "%d / %d\n", pn, cn);
+    for (i = 0; i < pn; ++i)
+        fprintf(fo, "%.8lf, %.8lf, %d\n", data[i].getX(), data[i].getY(),
+                coloring[i]);
+
+    /* Free the resources and return. */
+    delete[](data);
+    delete[](coloring);
+    delete[](mean);
+    fclose(fi);
+    fclose(fo);
+    return 0;
+}
+
+
+/*********************************************************
+           Feel free to modify the things below
+ *********************************************************/
+
+/*
+ * K-Means algorithm clustering. Originally implemented in a traditional
+ *   sequential way. You should optimize and parallelize it for a better
+ *   performance. Techniques you can use include but not limited to:
+ *
+ *     1. OpenMP shared-memory parallelization.
+ *     2. SSE SIMD instructions.
+ *     3. Cache optimizations.
+ *     4. Manually using pthread.
+ *     5. ...
+ *
+ */
+
+/* Get distance between two points */
+inline static double _m_mm_pt_dis(const point_t& p1, const point_t& p2)
+{
+    __m128d a = _mm_loadu_pd((double*)&p1);
+    __m128d b = _mm_loadu_pd((double*)&p2);
+
+    __m128d diff = _mm_sub_pd(a, b);
+    diff = _mm_mul_pd(diff, diff);
+
+    double diffArr[2];
+    _mm_storeu_pd(diffArr, diff);
+
+    return diffArr[0] + diffArr[1];
+}
+
+void
+kmeans (point_t * const data, point_t * const mean, color_t * const coloring,
+        const int pn, const int cn)
+{
+    bool converge;
+    int num_threads = omp_get_max_threads();
+
+    /* Loop through the following two stages until no point changes its color
+       during an iteration. */
+    do {
+        /* Conduct a distributive k-means algorithm, thus, allocate a t by c
+           matrix to accommodate distributive results, where t is the number
+           of threads, c is the number of centers.
+           More about the major order of matrix: the distributive algorithm
+           saves centers for one threads in one row, which could effectively
+           avoid cache coherence issues, as the different threads hold different
+           continuous memory space. */
+
+        // Reduction vectors
+        point_t* agg_sum = new point_t[cn];             // Aggregation summation of each parition
+        int* agg_count = new int[cn];                   // Aggregation summation of counts
+        converge = true;                                // Aggregation converge flag
+        for (int i = 0; i < cn; i++) agg_count[i] = 0;
+
+        // Record the total number of threads
+        // int thread_num = 0;
+
+        /* Map: Compute the color of each point. A point gets assigned to the
+           cluster with the nearest center point. */
+        omp_set_num_threads(num_threads);
+        #pragma omp parallel
+        {
+            // #pragma omp master
+            // thread_num = omp_get_num_threads();
+
+            // int id = omp_get_thread_num();
+
+            // Map matrices
+            point_t* sums = new point_t[cn];  // Accumulation for each partition
+            int* counts = new int[cn];        // Counts of accumulation for each partition
+            for (int i = 0; i < cn; i++) counts[i] = 0;
+            bool localConverge = true;        // Whether all points in current partition is convergent
+            // Note that, here reduce converge for each threads
+            #pragma omp for
+            for (int i = 0; i < pn; i += 2)
+            {
+                color_t new_colors[2] = {color_t(cn), color_t(cn)};
+
+                double min_dists[2] = {
+                    std::numeric_limits<double>::infinity(),
+                    std::numeric_limits<double>::infinity()
+                };
+                // __m128d m_min_dists = _mm_load_pd(min_dists);
+
+                for (color_t c = 0; c < cn; ++c)
+                {
+                    // double dist = pow(data[i].getX() - mean[c].getX(), 2) + pow(data[i].getY() - mean[c].getY(), 2);
+                    // double dist = _m_mm_pt_dis(data[i], mean[c]);
+
+                    // double mean_xy[4] = {mean[c].getX(), mean[c].getY(), mean[c + 1].getX(), mean[c + 1].getY()};
+                    
+                    __m256d m_data_xy = _mm256_load_pd((double*)&data[i]);
+                    __m256d m_mean_xy = _mm256_broadcast_pd((__m128d*)(&mean[c]));
+                    // __m256d m_mean_xy = _mm256_load_pd((double*)mean_xy);
+                    __m256d m_dist_xy = _mm256_sub_pd(m_data_xy, m_mean_xy);
+                    m_dist_xy = _mm256_mul_pd(m_dist_xy, m_dist_xy);
+                    __m256d m_dists = _mm256_hadd_pd(m_dist_xy, m_dist_xy);
+                    // __m128d m_dists = _mm256_extractf128_pd(_mm256_permute4x64_pd(
+                    //     _mm256_hadd_pd(m_dist_xy, m_dist_xy),
+                    //     0x8
+                    // ), 0);
+
+                    // __m128d m_cmp_res = _mm_cmplt_pd(m_dists, m_min_dists);
+                    // m_min_dists = _mm_or_pd(
+                    //     _mm_and_pd(
+                    //         m_min_dists,
+                    //         _mm_xor_pd(m_cmp_res, _mm_set_pd1((double)0xFFFFFFFFFFFFFFFF))
+                    //     ),
+                    //     _mm_and_pd(
+                    //         m_dists,
+                    //         m_cmp_res
+                    //     )
+                    // );
+                    double dists[4] = {
+                        std::numeric_limits<double>::infinity(),
+                        std::numeric_limits<double>::infinity(),
+                        std::numeric_limits<double>::infinity(),
+                        std::numeric_limits<double>::infinity()
+                    };
+                    _mm256_store_pd(dists, m_dists);
+                    
+                    if (dists[0] < min_dists[0])
+                    {
+                        min_dists[0] = dists[0];
+                        // new_colors[0] = c;
+                    }
+                    if (dists[2] < min_dists[1])
+                    {
+                        min_dists[1] = dists[2];
+                        // new_colors[1] = c;
+                    }
+                    
+
+
+                    // if (dist < min_dist)
+                    // {
+                    //     min_dist = dist;
+                    //     new_color = c;
+                    // }
+                }
+
+                if (coloring[i] != new_colors[0])
+                {
+                    coloring[i] = new_colors[0];
+                    localConverge = false;
+                }
+
+                if (coloring[i+1] != new_colors[1])
+                {
+                    coloring[i+1] = new_colors[1];
+                    localConverge = false;
+                }
+
+                // Now accumulate points for each centers
+                // sums[idx].setXY(
+                //     sums[idx].getX() + data[i].getX()
+                //     , sums[idx].getY() + data[i].getY()
+                // );
+                sums[new_colors[0]] += data[i];
+                sums[new_colors[1]] += data[i + 1];
+                // Accumulate counts for reduction
+                counts[new_colors[0]]++;
+                counts[new_colors[1]]++;
+            }
+
+            #pragma omp critical
+            for (int i = 0; i < cn; i++)
+            {
+                agg_sum[i] += sums[i];
+                agg_count[i] += counts[i];
+                converge &= localConverge;
+            }
+
+            delete[] sums;
+            delete[] counts;
+        }
+        
+        /* Reduction: Calculate the new mean for each cluster to be the
+           current average of point positions in the cluster. */
+        for(int i = 0; i < cn; i++) {
+            mean[i] = agg_sum[i] / agg_count[i];
+        }
+
+        delete[] agg_count;
+        delete[] agg_sum;
+
+    } while (!converge);
+}
+
+/*********************************************************
+                           End
+ *********************************************************/
